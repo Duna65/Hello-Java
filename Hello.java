a44124eddfb9a2d0b5ae823aff38f719a469a180
Duna65 <142834133+Duna65@users.noreply.github.com>
2024-01-24 14:16:05 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,29 @@
+import javax.swing.DefaultListCellRenderer;
+
+/**
+ * Hello
+ */
+public class Hello {
+
+    
+    public static void main(String[] args) {
+        String N1="Dallin(me)";
+        String N2="Brett";
+        String N3="Spencer";
+        String N4="Drew(oldest)";
+        String N5="Tucker";
+        String N6="Lydia(youngest)";
+        String N7="Kay(mom)";
+        String N8="Adam(dad)";
+
+    System.out.println(N1);
+    System.out.println(N2);
+    System.out.println(N3);
+    System.out.println(N4);
+    System.out.println(N5);
+    System.out.println(N6);
+    System.out.println(N7);
+    System.out.println(N8);
+    System.out.println("This is my family, we live in New Plymouth, Idaho");
+    
+}}
